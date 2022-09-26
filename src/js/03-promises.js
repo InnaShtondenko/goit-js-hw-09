59f8c3e6247412d9d04caeae418eaaec51e5101b
InnaShtondenko <inna.mycase@gmail.com>
2022-09-26 22:34:28 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,29 +1,33 @@
 import Notiflix, { Notify } from 'notiflix';
 
 const form = document.querySelector('.form');
-const promisesOpt = {
-  delay: 0,
-  step: 0,
-  amount: 0,
+const promisesEl = {
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
 };
 
-(() => {
-  form.addEventListener('submit', onFormSubmit);
-})();
 
-function onFormSubmit(event) {
+form.addEventListener('click', (event) => {
   event.preventDefault();
-  
+  let delay = Number(promisesEl.delay.value);
+  let step = Number(promisesEl.step.value);
+  let amount = Number(promisesEl.amount.value);
+
+
+  for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
-      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
   
-delay += step;
-}
+    delay += step;
+  }
+  
+});
   
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
